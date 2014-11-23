5373e772a903aac1e92b81773a3e261c731ad212
Manuel Valls Fernández <manolo@chatiku.com>
2014-11-23 17:22:28 +0100

    Added unicode flag to RegExp

--- main/RegExp.js
+++ main/RegExp.js
@@ -8,6 +8,7 @@ ebjs.define(RegExp,12,function*(buff,data){
   if(data.global)     flags |= 2;
   if(data.ignoreCase) flags |= 4;
   if(data.sticky)     flags |= 8;
+  if(data.unicode)    flags |= 16;
   
   yield buff.pack(Number,flags);
   yield buff.pack(String,data.source);
@@ -19,10 +20,11 @@ ebjs.define(RegExp,12,function*(buff,data){
   
   fn = yield buff.unpack(Number);
   
-  if(fn & 1) flags += 'm';
-  if(fn & 2) flags += 'g';
-  if(fn & 4) flags += 'i';
-  if(fn & 8) flags += 'y';
+  if(fn & 1)  flags += 'm';
+  if(fn & 2)  flags += 'g';
+  if(fn & 4)  flags += 'i';
+  if(fn & 8)  flags += 'y';
+  if(fn & 16) flags += 'u';
   
   re = new RegExp(yield buff.unpack(String),flags);
   re.lastIndex = yield buff.unpack(Number);

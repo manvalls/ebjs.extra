8cb14f28bd3466e3c0e0dd9a50a90b1824096df5
Manuel Valls Fernández <manolo@chatiku.com>
2014-12-18 22:55:20 +0100

    Fixes #2

--- main/Error.js
+++ main/Error.js
@@ -1,12 +1,17 @@
 var ebjs = require('ebjs');
 
-ebjs.define(Error,14,function*(data){
-  
+function* packer(data){
   yield this.pack(String,data.message);
-  
-},function*(){
-  
-  return new Error(yield this.unpack(String));
-  
-});
+}
 
+function* unpacker(Constructor){
+  return new Constructor(yield this.unpack(String));
+}
+
+ebjs.define(Error,          14,packer,unpacker);
+ebjs.define(EvalError,      15,packer,unpacker);
+ebjs.define(RangeError,     16,packer,unpacker);
+ebjs.define(ReferenceError, 17,packer,unpacker);
+ebjs.define(SyntaxError,    18,packer,unpacker);
+ebjs.define(TypeError,      19,packer,unpacker);
+ebjs.define(URIError,       20,packer,unpacker);
